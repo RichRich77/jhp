4429a6ecf1b9f600d67b0e1557077f872805bcd5
Bijan Parvin <bparvin12@gmail.com>
2019-01-14 11:29:36 -0800

    created table

---
+++ models/clothing.js
@@ -0,0 +1,50 @@
+module.exports = function (sequelize, DataTypes) {
+    var Clothing = sequelize.define("Clothing", {
+        clothing_type: {
+            type: DataTypes.ENUM,
+            allowNull: false,
+            values: ["shirt", "short", "pant", "sock", "underwear"],
+        },
+        gender: {
+            type: DataTypes.ENUM,
+            allowNull: false,
+            values: ["male", "female", "unisex"],
+        },
+        small_quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 100,
+            validate: {
+                isNumeric: true,
+            }
+        },
+        medium_quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 100,
+            validate: {
+                isNumeric: true,
+            }
+        },
+        large_quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 100,
+            validate: {
+                isNumeric: true,
+            }
+        },
+        color: {
+            type: DataTypes.ENUM,
+            allowNull: false,
+            values: ["white", "black", "blue", "red", "green"],
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            validate: {
+                isNumeric: true
+            }
+        }
+    });
+}
