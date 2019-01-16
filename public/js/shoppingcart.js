95562feedb19a17cf65f3f94743a2dd18d782ec5
michelle <michellemchoe@gmail.com>
2019-01-16 10:09:05 -0800

    daily merge

--- public/js/shoppingcart.js
+++ public/js/shoppingcart.js
@@ -1,5 +1,11 @@
-var numItems = 0; // number of items in cart
-$("#numItems").text(4);
+var numItems; // number of items in cart
+$("#numItems").text(4); // update 
 $("#cartSubtotal").text(234.43);
 $("#cartTax").text((234.43 * 0.075).toFixed(2));
-$("#cartTotal").text((0 +(234.43 * 1.075)).toFixed(2));
+$("#cartTotal").text((0 + (234.43 * 1.075)).toFixed(2));
+
+// add onclick for adding to cart to increment numitems however many
+
+for (var i = 1; i <= numItems; i++){
+    addToBagList()
+}
