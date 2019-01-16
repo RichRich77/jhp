a3bd5f732ca1a6b87b67484a0219ca7ab541e883
RichRich77 <rmartinezcomputer@gmail.com>
2019-01-16 09:56:52 -0800

    Jan 16

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ module.exports = function(sequelize, DataTypes) {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          is: '^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8,20}$'
+          // is: '^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8,20}$'
         }
       },
     });
