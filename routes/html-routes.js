7aa749384c4a975cd7168c79b1b8162cf89fd33f
RichRich77 <rmartinezcomputer@gmail.com>
2019-01-17 23:13:11 -0800

    change routes in routes/html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/user");
     }
     res.sendFile(path.join(__dirname, "../public/user.html"));
   });
@@ -17,15 +17,15 @@ module.exports = function(app) {
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/user");
     }
     res.sendFile(path.join(__dirname, "../public/user.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page '/members'
+  app.get("/user", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/user.html"));
   });
 
 };
