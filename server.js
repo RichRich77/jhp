80080698f498074c207dec435a76c7512ab48878
RichRich77 <rmartinezcomputer@gmail.com>
2019-01-17 22:17:16 -0800

    server.js setup for auth functionality

--- server.js
+++ server.js
@@ -5,11 +5,18 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var passport = require("./config/passport");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
 var PORT = process.env.PORT || 3000;
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -17,6 +24,11 @@ var db = require("./models");
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Static directory
 app.use(express.static("public"));
@@ -26,6 +38,9 @@ app.use(express.static("public"));
 require("./controller/clothing-api-routes.js")(app);
 require("./controller/user-api-routes.js")(app);
 require("./controller/html-routes.js")(app);
+
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
