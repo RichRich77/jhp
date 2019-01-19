d9723d1442808241305d2f689b797c7dd9fa8577
RichRich77 <rmartinezcomputer@gmail.com>
2019-01-18 16:31:11 -0800

    user auth messaging front end

--- public/js/user.js
+++ public/js/user.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
 var nameInput = $("#user-name");
 var emailInput = $("#email-input");
 var passwordInput = $("#password-input");
+var emailInput2 = $("#email-input2");
+var passwordInput2 = $("#password-input2");
 
  $(document).on("submit", ".register-form", handleUserFormSubmit);
 
@@ -18,18 +20,31 @@ var passwordInput = $("#password-input");
     //     password: passwordInput.val().trim()
     //   };
 
+    if (!$(".form").is(':animated') && !emailInput2.val().trim().trim() && !passwordInput2.val().trim().trim()) {
+           return;
+        }
+        var successMessage2 = '<a href="/"><h3>Success!</h3><h3>Click to Continue</h3></a>';
+        $('.message').html(successMessage2);
+        
+
+    }
+    
+    
      if(!nameInput.val().trim().trim() && !emailInput.val().trim().trim() && !passwordInput.val().trim().trim()) {
          return;
      }
+     var successMessage = '<a href="/"><h3>Success!</h3><h3>Click to Continue</h3></a>';
+     $('.message').html(successMessage);
 
      upsertUser({
          name: nameInput.val().trim(),
          email: emailInput.val().trim(),
          password: passwordInput.val().trim()
+     
      });
      
-    //  $('#log-info').html("Logged In");
- }
+
+ 
 
  function upsertUser(userData) {
      $.post("api/users", userData)
