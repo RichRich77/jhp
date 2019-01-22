5c99e99ae8f3d354b87c8022be28fb00ca8d61df
michelle <michellemchoe@gmail.com>
2019-01-22 10:02:21 -0800

    literally in the car on the garage roof trying to submit this. sorry. traffic

--- public/js/add-to-cart.js
+++ public/js/add-to-cart.js
@@ -2,69 +2,108 @@
 $(document).ready(function () {
 
     // When user clicks addToCart
-    $("#addToBag").click(function() {
+    $("#addToBag").click(function () {
         // event.preventDefault();
 
         // // Get basic info about selection
-        var sIID = $(this).parent().attr("id").trim();
+        var sIProdID = $(this).parent().attr("id").trim();
         var sIQty = $(this).parent().find("#itemQty").text().trim();
         var sISize = "small".trim();
         var sIColor = "blue".trim();
-console.log("made");
+
+        $.get("/api/clothing" + sIProdID, function (data) {
+            connection.query("SELECT * FROM clothings where id = ?", sIProdID, function (err, result) {
+                var sIType = result.clothing_type;
+                var sIGender = result.gender;
+                //check for stock
+                switch (sISize) {
+                    case small:
+                        if (sIQty >= result.small_quantity) {
+                            
+                        }
+                        break;
+                    case medium:
+                        if (sIQty >= result.medium_quantity) {
+                            return true;
+                        }
+                        break;
+                    case large:
+                        if (sIQty >= result.large_quantity) {
+                            return true;
+                        }
+                        break;
+                    default:
+                        console.log("ERROR");
+                        return false;
+                        break;
+                }
+
+
+                for (var i = 0; i < result.length; i++) {
+                    html += "<li><p> ID: " + result[i].id + "</p>";
+                    html += "<p> Name: " + result[i].name + "</p>";
+                    html += "<p> Coolness Points: " + result[i].coolness_points + "</p>";
+                    html += "<p>Attitude: " + result[i].attitude + "</p></li>";
+                }
+
+                html += "</ul>";
+
+                res.send(html);
+            }
 
         // Make a new cart item object
         var newCartItem = {
-            productID: parseInt(sIID),
-            selectedSize: sISize,
-            selectedQty: parseInt(sIQty),
-            color: sIColor            
-        };
+                productID: parseInt(sIProdID),
+                selectedSize: sISize,
+                selectedQty: parseInt(sIQty),
+                color: sIColor
+            };
 
-        // Send an AJAX POST-request with jQuery
-        $.post("/api/shoppingcart", newCartItem)
-        .then (function(data){
-            console.log(data);
+            // Send an AJAX POST-request with jQuery
+            $.post("/api/shoppingcart", newCartItem)
+                .then(function (data) {
+                    console.log(data);
+                });
+
         });
 
+        // get size desired
+        $(".size-selection").click(function () {
+            var selSize = $(this).attr("id");
+            $(this).parent().innerHTML = selSize;
+
+            // TODO
+            // set ID for grandparent to size so we can access it later
+        });
+
+        // set desired color
+        $(".color-selection").click(function () {
+            var selColor = $(this).attr("id");
+            var curVal = $(this).parent().parent().attr("value");
+            // console.log("currval = " + curVal + "   selVal = " + selColor);
+
+            // TODO
+            // manage to get the darn color
+        });
+
+
+        // decrement particular item before adding to cart
+        $("#decItemQty").click(function () {
+            var curQty = $(this).siblings("#itemQty").text();
+            console.log(curQty + "-");
+            if (curQty > 0) {
+                curQty--;
+                $(this).siblings("#itemQty").text(curQty);
+            }
+        });
+
+        // increment particular item before adding to cart
+        $("#incItemQty").click(function () {
+            var curQty = $(this).siblings("#itemQty").text();
+            // var stockCheckID = $(this).parent().parent().parent().val();
+            if (true) { //checkStock(stockCheckID)){
+                curQty++;
+                $(this).siblings("#itemQty").text(curQty);
+            }
+        });
     });
-
-    // get size desired
-    $(".size-selection").click(function () {
-        var selSize = $(this).attr("id");
-        $(this).parent().innerHTML = selSize;
-    
-    // TODO
-        // set ID for grandparent to size so we can access it later
-    });
-
-    // set desired color
-    $(".color-selection").click(function() {
-        var selColor = $(this).attr("id");
-        var curVal = $(this).parent().parent().attr("value");
-        // console.log("currval = " + curVal + "   selVal = " + selColor);
-
-    // TODO
-        // manage to get the darn color
-    });
-
-
-    // decrement particular item before adding to cart
-    $("#decItemQty").click(function(){
-        var curQty = $(this).siblings("#itemQty").text();
-        console.log(curQty + "-");
-        if (curQty > 0){
-            curQty--;
-            $(this).siblings("#itemQty").text(curQty);
-        }
-    });
-
-    // increment particular item before adding to cart
-    $("#incItemQty").click(function(){
-        var curQty = $(this).siblings("#itemQty").text();
-        // var stockCheckID = $(this).parent().parent().parent().val();
-        if (true){ //checkStock(stockCheckID)){
-            curQty++;
-            $(this).siblings("#itemQty").text(curQty);
-        }
-    });
-});
--- public/js/shoppingcart.js
+++ public/js/shoppingcart.js
@@ -1,123 +1,32 @@
-// $(document).ready(function() {
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $("#deleteCartItem").on("click", function(event) {
+      var id = $(this).data("id");
   
-// });
-
-
-
-// // var numItems; // number of items in cart
-// // $("#cartSubtotal").text(234.43);
-// // $("#cartTax").text((234.43 * 0.075).toFixed(2));
-// // $("#cartTotal").text((0 + (234.43 * 1.075)).toFixed(2));
-
-// // // add onclick for adding to cart     to increment numitems however many
-
-
-// // // When the page loads, grab and display all of our chirps
-// // // $.get("/shoppingcart", function(data) { //pretty sure gotta change this line
-
-// // //     if (data.length !== 0) {
+      // Send the PUT request.
+      $.ajax("/api/shoppingcart/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("Cart Item Was Deleted.");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
   
-// // //       for (var i = 0; i < data.length; i++) {
   
-
-
-
-
-
-
-// // //         var row = $("<div>");
-// // //         row.addClass("chirp");
+      // Send the POST request.
+      $.ajax("/api/cats", {
+        type: "POST",
+        data: newCat
+      }).then(
+        function() {
+          console.log("created new cat");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
   
-// // //         row.append("<p>" + data[i].author + " chirped.. </p>");
-// // //         row.append("<p>" + data[i].body + "</p>");
-// // //         row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
-  
-// // //         $("#chirp-area").prepend(row);
-  
-// // //       }
-  
-// // //     }
-  
-// // //   });
-  
-
-
-    
-
-    
-// // function loadShoppingCart() {
-// //     // display how many items in cart
-// //     $("#numItems").text(numItems); // update 
-
-// //     // left join. and then use new joined shopping cart to do everything
-// //     // SELECT * FROM ShoppingCart
-// //     // LEFT JOIN Clothing ON ShoppingCart.ProductID = Clothing.ID
-// //     // ORDER BY ShoppingCart.ID;
-
-// //     for (var i = 1; i <= numItems; i++) {
-// //         SELECT 
-// //         addToBagList();
-// //     }
-
-
-
-
-
-// // // }
-// // $(document).ready(function () {
-
-// //     var smallInput = $("#smallInput");
-// //     var mediumInput = $("#mediumInput");
-// //     var largeInput = $("#largeInpt");
-// //     var oneQuant = $("#one");
-// //     var twoQuant = $("#two");
-// //     var threeQuant = $("#three");
-// //     var fourQuant = $("#four");
-// //     var fiveQuant = $("#five");
-// //     var quantity = $("#quantity");
-// //     var black = $("#black");
-// //     var red = $("#red");
-// //     var white = $("#white");
-// //     var blue = $("#blue");
-// //     var green = $("#green");
-
-
-// //     $(function () {
-// //         $('.button').click(function () {
-// //             $(this).toggleClass('on');
-// //         });
-// //     });
-
-// //     $(document).on("click", "#addToBag", handleShirtSubmit);
-
-// //     function handleShirtSubmit(event) {
-// //         event.preventDefault();
-
-// //         // if ((!smallInput || !mediumInput || !largeInput) &&
-// //         //     (!oneQuant || !twoQuant || !threeQuant || !fourQuant || !fiveQuant) &&
-// //         //     (!black || !red || !white || !blue || !green)) {
-// //         //     return;
-// //         // }
-
-// //         upsertItem({
-// //             clothing_type: "shirt",
-// //             gender: "men",
-// //             small_quantity: 1,
-// //             medium_quantity: 0,
-// //             large_quantity: 0,
-// //             color: "white",
-// //             price: 54.99,
-// //             review: "5",
-// //             image: "https://dtpmhvbsmffsz.cloudfront.net/posts/2016/09/11/57d5cb5e2599fe3e2a00776d/m_57d5cb5e2599fe3e2a00776e.jpg"
-// //         });
-// //     }
-
-// //     function upsertItem(itemData) {
-// //         $.post("/api/shoppingcart", itemData)
-// //             .then(getItems);
-// //     }
-
-// //     function getItems() {
-// //         $.get("/api/shoppingcart", itemData);
-// //     }
-// // });
